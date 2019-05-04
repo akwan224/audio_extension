f6e16e829bf5a929c9e32a6edbabef2b190203c2
Alvin Kwan <alvinkwan24@gmail.com>
2019-05-04 16:44:39 -0700

    Update cont.js

--- cont.js
+++ cont.js
@@ -1,16 +1,59 @@
-alert("Click anywhere to esektit")
-
-
-// Used like so
-// var esk = [1, 2, 3, 4, 5 , 6 , 7 , 8 , 9 , 10];
-// esk = shuffle(arr);
-
+alert("Click anywhere to play jingle")
 
 
 document.addEventListener('click', () => {
-  let url = chrome.runtime.getURL("esk1.mp3"); 
+		var rand = [1, 2, 3];	
+		rand = shuffle(rand); 
+		var ran = rand[1]; 
+		if(ran = 1) {
+			var random = [1, 2, 3, 4, 5 , 6 , 7 , 8 , 9 , 10];
+			random = shuffle(random);
+			var str1 = "jingle";
+			var str2 = random[1];  
+			var str3 = ".mp3";
+			var audioClip = str1.concat(str2, str3);
+			var ac = audioClip.toString(); 
+		}
+		if(ran = 2) {
+			var random = [1, 2, 3, 4, 5 , 6 , 7 , 8];
+			random = shuffle(random);
+			var str1 = "song";
+			var str2 = random[1];  
+			var str3 = ".mp3";
+			var audioClip = str1.concat(str2, str3);
+			var ac = audioClip.toString(); 
+		}
+		if(ran = 3) {
+			var random = [1, 2, 3, 4, 5 , 6 , 7 , 8 , 9 , 10, 11, 12, 13, 14, 15, 16, 17];
+			random = shuffle(random);
+			var str1 = "speech";
+			var str2 = random[1];  
+			var str3 = ".mp3";
+			var audioClip = str1.concat(str2, str3);
+			var ac = audioClip.toString(); 
+		}
+	let url = chrome.runtime.getURL(ac); 
 	console.log(url)
-
 	let a = new Audio(url)
 	a.play()
+// }
 })
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
